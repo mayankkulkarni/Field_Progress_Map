241453af88d8d51360a3413b66c1a0a9398b0dee
Uche Uba <ucheuba@usc-guestwireless-upc-newsc10258.usc.edu>
2019-12-05 19:10:38 -0800

    Deleted old frontend files

--- src/clustering.py
+++
@@ -1,9 +0,0 @@
-from sklearn.cluster import KMeans
-
-def create_clusters(dataframe, volunteers):
-    kmeans = KMeans(n_clusters=volunteers).fit(dataframe)
-    return kmeans
-
-
-def get_centroids(kmeans):
-    return kmeans.cluster_centers_
